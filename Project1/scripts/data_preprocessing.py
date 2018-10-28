196795b744f5bca20d18cd59d8aa3a51ccad9ada
Lucas Ramirez <lucas.ramirez@live.fr>
2018-10-28 23:23:23 +0100

    Added documentation

--- Project1/scripts/data_preprocessing.py
+++ Project1/scripts/data_preprocessing.py
@@ -3,21 +3,36 @@
 
 import numpy as np
 
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    poly = np.ones((len(x), 1))
+def build_poly(tx, degree):
+    """Pre-processes the data by polynomial expansion. 
+    
+    Args:
+        tx (N x D matrix): Features matrix (not pre-processed).
+        degree (int): Expansion degree.
+    Returns:
+        (N x (1 + (D * degree)) matrix): Extended features matrix.
+    """    
+    poly = np.ones((len(tx), 1))
     for deg in range(1, degree+1):
-        poly = np.c_[poly, np.power(x, deg)]
+        poly = np.c_[poly, np.power(tx, deg)]
     return poly
 
-def build_poly_diff_degrees(x, degrees):
-    """"""
-    if len(x[0]) != len(degrees):
+def build_poly_diff_degrees(tx, degrees):
+    """Pre-processes the data by polynomial expansion, with a different
+    degree for each feature. 
+    
+    Args:
+        tx (N x D matrix): Features matrix (not pre-processed).
+        degrees (int): Expansion degree for each feature.
+    Returns:
+        Extended features matrix.
+    """
+    if len(tx[0]) != len(degrees):
         raise ValueError("Array of degrees must be of same length that columns of data")
 
-    poly_diff = np.ones((len(x), 1))
+    poly_diff = np.ones((len(tx), 1))
     for ind, degree in enumerate(degrees):
-        poly = build_poly(x[:, ind], int(degree))
+        poly = build_poly(tx[:, ind], int(degree))
         poly_diff = np.append(poly_diff, poly[:, 1:], axis=1)
     
     return poly_diff
