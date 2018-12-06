4177a9cbdf0b54dddcdf15845473d0cd606a0ffb
Manuel Cordova <manuel.cordova@epfl.ch>
2018-12-06 16:59:13 +0100

    Mettre un CPU en pls 101

--- Project2/working_directories/Manuel/testCNN_200_oneHot.py
+++ Project2/working_directories/Manuel/testCNN_200_oneHot.py
@@ -26,11 +26,12 @@ from img_load import *
 from modelCNN_200 import *
 from submission import *
 from shutil import copyfile
+import pickle
 
 # Make script reproducible
 random.seed(1)
 
-nTrain = 200
+nTrain = 90
 nAdd = 100
 
 im_height = 200
@@ -42,16 +43,22 @@ selection_threshold = 0.7
 
 # Loaded a set of images
 
+print("Loading training images...")
 root_dir = "../../project_files/data/"
 training_dir = root_dir + "training/"
+valid_dir = root_dir + "validation/"
 imgs, gt_imgs = load_training(training_dir, nTrain)
 resized_imgs = np.asarray(resize_imgs(imgs,im_height,im_width))
-
-plt.imshow(resized_imgs[13])
-
 onehot_gt_imgs = convert_to_one_hot(gt_imgs)
 resized_onehot_gt_imgs = np.asarray(resize_binary_imgs(onehot_gt_imgs,gt_height,gt_width,0.25))
-plt.imshow(gt_imgs[13])
+print("Done!")
+
+print("Loading validation images...")
+val_imgs, val_gt_imgs = load_training(valid_dir, nTrain)
+resized_val_imgs = np.asarray(resize_imgs(val_imgs,im_height,im_width))
+onehot_val_gt_imgs = convert_to_one_hot(val_gt_imgs)
+resized_onehot_val_gt_imgs = np.asarray(resize_binary_imgs(onehot_val_gt_imgs,gt_height,gt_width,0.25))
+print("Done!")
 
 add_dir = root_dir + "additionalDataset/"
 
@@ -75,13 +82,18 @@ epochs = 20
 num_classes = 2
 alpha = 0.01
 batch_size = 32
-num_iters = 10
+num_iters = 20
 
 input_img = Input((im_height, im_width, 3), name='img')
 model = get_unet(input_img, num_classes, n_filters=16, dropout=0.25, batchnorm=True)
 
+model_accuracy = []
+model_loss = []
+model_val_accuracy = []
+model_val_loss = []
 for i in range(num_iters):
     print("Iteration " + str(i+1) + "...")
+    print("Loading additional training images...")
     add_imgs, add_gt_imgs = load_training(add_dir, nAdd, shuffle=True)
     add_img_patches = make_patches(add_imgs,im_height,im_width)
     add_gt_patches = make_patches(add_gt_imgs,im_height,im_width)
@@ -89,17 +101,18 @@ for i in range(num_iters):
     sel_add_imgs, sel_add_gts = select_imgs(add_img_patches, add_onehot_gt_patches, selection_threshold)
     all_train_imgs = np.append(resized_imgs, sel_add_imgs,axis=0)
     all_train_gts = np.append(resized_onehot_gt_imgs, sel_add_gts,axis=0)
-    X_train, X_valid, y_train, y_valid = train_test_split(all_train_imgs, all_train_gts, test_size=0.10)
-    if os.path.isfile('test_CNN_200.h5'):
-        model.load_weights('test_CNN_200.h5')
-    model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
-    model.summary()
-    callbacks = [EarlyStopping(patience=2, verbose=1),ReduceLROnPlateau(factor=0.1, patience=2, min_lr=0.00001, verbose=1),ModelCheckpoint('test_CNN_200_'+str(i)+'_{epoch:03d}.h5', verbose=1, save_best_only=True, save_weights_only=True)]
-    model_train = model.fit(X_train, y_train, batch_size=batch_size,epochs=epochs,callbacks=callbacks,verbose=1,validation_data=(X_valid, y_valid))
-    copyfile('test_CNN_200.h5', 'test_CNN_200_'+str(i)+'.h5')
     print("Done!")
-
+    if os.path.isfile('test_CNN_200_oneHot.h5'):
+        model.load_weights('test_CNN_200_oneHot.h5')
+    model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=[f1_score])
+    callbacks = [EarlyStopping(patience=2, verbose=1),ReduceLROnPlateau(factor=0.1, patience=2, min_lr=0.00001, verbose=1),ModelCheckpoint('test_CNN_200_oneHot.h5', verbose=1, save_best_only=True, save_weights_only=True)]
+    model_train = model.fit(all_train_imgs, all_train_gts, batch_size=batch_size,epochs=epochs,callbacks=callbacks,verbose=1,validation_data=(resized_val_imgs, resized_onehot_val_gt_imgs))
+    with open('CNN_200_onehot_'+str(i+1)+'.history', 'wb') as file_pi:
+        pickle.dump(model_train.history, file_pi)
+    print("Iteration " + str(i+1) + " done!")
 print("Training done!")
+
+
 
 #test_dir = root_dir + "test_set_images/"
 #resized_test_imgs = load_test(test_dir)
