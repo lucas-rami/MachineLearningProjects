ae0f83f8f487a79956b126c1fc192afdb5df3da7
Lucas Ramirez <lucas.ramirez@live.fr>
2018-12-11 13:25:46 +0100

    Documentation for img_patch

--- Project2/src/patch.py
+++ Project2/src/patch.py
@@ -6,6 +6,23 @@ import numpy as np
 MIN_PATCH_SIZE = 16
 
 def img_patch(image, patch_size):
+    """Separates `image` into multiple patches of size (`patch_size` * `patch_size`).
+
+    If the `patch_size` is bigger than the image's width or height then it is reduced
+    to `min(width,height)`. If the `patch_size` doesn't divide the image size perfectly 
+    then the function handles patch overlapping correctly and returns an "overlap image"
+    which is as big as the original image and in which each pixel contains the number
+    of patches in which it can be found. After re-constructing the image from its patches
+    the resulting image can be divided by the "overlap image" to obtain real values for all
+    pixels. 
+
+    Args:
+        image (width * height * X tensor): An image.
+        patch_size (int): The patch size to use to make patches out of the image.
+    Returns:
+        nb_patch * patch_size * patch_size * X tensor : The array of patches for the original image.
+        width * height tensor : An "overlap image" to be used during image recontruction from patches.
+    """
 
     # Argument checking
     if patch_size < MIN_PATCH_SIZE:
@@ -66,9 +83,9 @@ def img_patch(image, patch_size):
                 h_bound_high = width
 
             # Create new patch
-            patches.append(image[ h_bound_low:h_bound_high , v_bound_low:v_bound_high ])
+            patches.append(image[ h_bound_low:h_bound_high , v_bound_low:v_bound_high , : ])
             
-    return patches, overlap_image
+    return np.asarray(patches), overlap_image
    
 
 
