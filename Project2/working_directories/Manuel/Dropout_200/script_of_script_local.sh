36ab7117bbc14b85cc7462126946c5329d918606
Lucas Ramirez <lucas.ramirez@epfl.ch>
2018-12-20 23:00:59 +0100

    Deleted working_directories

--- Project2/working_directories/Manuel/Dropout_200/script_of_script_local.sh
+++
@@ -1,8 +0,0 @@
-#bin/bash
-
-for i in `seq 1 9`
-do
-  echo "Dropout: 0.${i}"
-  sed "s/Dropout = 0/Dropout = 0.${i}/g" UNET_patch_120_template.py > UNET_patch_120_Dropout_0.${i}.py
-  sed "s/python3/python3 testCNN_200_Dropout_0.${i}.py/g" test_script_template.sh > test_script_0.${i}.sh
-done
